df162afcb7a40de2e0b410ea6ac4492a9c3a9617
Jern_cp <jesper@niedermann.dk>
2009-06-14 07:38:43 +0000

    Menus on SolutionFolders failed because they do not have a filename. This is solved by just returning Empty string as filename

--- deployMSI.bat
+++ deployMSI.bat
@@ -1,8 +1,5 @@
-xcopy /Y ManagedMenuExtensionsSetup\Release\ManagedMenuExtensionsSetup.msi "C:\MSI\ManagedMenuExtensionsSetup\"
-xcopy /Y ManagedMenuExtensionsSetup\Release\Setup.exe "C:\MSI\ManagedMenuExtensionsSetup\"
-xcopy /Y ManagedMenuVS2008Setup\Release\ManagedMenuVS2008Setup.msi "C:\MSI\ManagedMenuVS2008Setup\"
-xcopy /Y ManagedMenuVS2008Setup\Release\Setup.exe "C:\MSI\ManagedMenuVS2008Setup\"
-xcopy /Y ManagedMenuAddInSamplesSetup\Release\ManagedMenuAddInSamplesSetup.msi "C:\MSI\ManagedMenuAddInSamplesSetup\"
-xcopy /Y ManagedMenuAddInSamplesSetup\Release\Setup.exe "C:\MSI\ManagedMenuAddInSamplesSetup\"
+xcopy /Y ManagedMenuExtensionsSetup\Release\ManagedMenuExtensionsSetup.msi "C:\MSI\"
+xcopy /Y ManagedMenuVS2008Setup\Release\ManagedMenuVS2008Setup.msi "C:\MSI\"
+xcopy /Y ManagedMenuAddInSamplesSetup\Release\ManagedMenuAddInSamplesSetup.msi "C:\MSI\"
 
 
