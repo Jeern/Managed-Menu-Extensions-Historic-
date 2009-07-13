4665290be7310e3b0113f3307f419701e3cdb5b0
Jern_cp <SND\Jern_cp>
2009-07-13 11:58:03 +0000

    Addition of GlobalAssemblyInfo.cs

--- ManagedMenuAddInAdapters/Properties/AssemblyInfo.cs
+++ ManagedMenuAddInAdapters/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,28 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ManagedMenuAddInAdapters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jern")]
-[assembly: AssemblyProduct("ManagedMenuExtensions")]
-[assembly: AssemblyCopyright("Copyright © Jesper Niedermann 2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0a6118f2-b694-46ab-a559-a6ebb0c4b26d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.4.1")]
-[assembly: AssemblyFileVersion("0.1.4.1")]
--- ManagedMenuAddInSample2/Properties/AssemblyInfo.cs
+++ ManagedMenuAddInSample2/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,28 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ManagedMenuAddInSample2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jern")]
-[assembly: AssemblyProduct("ManagedMenuExtensions")]
-[assembly: AssemblyCopyright("Copyright © Jesper Niedermann 2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4c0e5ca8-28e6-4ffb-8d0b-ff3841eb2ea4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.4.1")]
-[assembly: AssemblyFileVersion("0.1.4.1")]
